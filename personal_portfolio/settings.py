bcff412b052535576292f678099226830d6032fa
Villy <vasilenkoilla898@gmail.com>
2023-03-30 18:37:48 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+h!^wg(-@$^=1*bu&v_z#nx-2pm@*v5rmqlb#ri@76n#1lhh!="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['villy.pythonanywhere.com']
 
 
 # Application definition
@@ -119,11 +119,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT= BASE_DIR / "static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URLS="media/"
+MEDIA_URL="media/"
 
 MEDIA_ROOT= BASE_DIR / "media"
